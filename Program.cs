ba08d3c3d2d771964c6ff8eb1f05b6a6613b069d
Marvin Roldan <marvinroldan_@hotmail.com>
2020-02-12 15:38:13 -0700

    Exercise 5

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp___Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
